9ad847dc894740d3bf928ea8315d7d9ad6f6a315
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-07-02 10:47:36 +0200

    advanced ts features

--- src/app.ts
+++ src/app.ts
@@ -1,64 +1,165 @@
-// type AddFn = (a: number, b: number) => number
-// in this case type is better than interface
-interface AddFn {
-  (a: number, b: number): number;
-}
-
-let add: AddFn = (n1, n2) => {
-  return n1 + n2;
-};
-
-// type User {
-//   name: string;
-//   age: number;
-//   greet(phrase: string): void;
-// }
-// we use interface for object
-
-interface User {
+type Admin = {
   name: string;
-  age: number;
-  greet(phrase: string): void;
-}
-
-let user1: User = {
-  name: 'Mathis',
-  age: 23,
-  greet(phrase) {
-    console.log(phrase + ' ' + this.name);
-  },
+  privileges: string[];
 };
 
-user1.greet('Hi there');
+type Employee = {
+  name: string;
+  startDate: Date;
+};
 
-interface Named {
-  readonly name?: string;
-  outputName?: string;
+// interface Admin {
+//   name: string;
+//   privileges: string[];
+// }
+
+// interface Employee {
+//   name: string;
+//   startDate: Date;
+// }
+
+// interface ElevatedEmployee extends Employee, Admin {};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: 'Max',
+  privileges: ['create-server'],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
+
+// TYPE GUARD & FUNCTION OVERLOAD
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
 }
 
-interface Greetable extends Named {
-  greet(phrase: string): void;
+// TS doesn't know if this is a number or a string
+const result1 = add(1, 5);
+const result2 = add('Mathis', 'Humbert') as string;
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name: ' + emp.name);
+  if ('privileges' in emp) {
+    console.log('Privileges: ' + emp.privileges);
+  }
+  if ('startDate' in emp) {
+    console.log('Start Date: ' + emp.startDate);
+  }
 }
 
-class Person implements Greetable {
-  name?: string;
-  age = 23;
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: 'Mathis', startDate: new Date() });
 
-  constructor(name?: string) {
-    if (name) {
-      this.name = name;
-    }
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...');
   }
 
-  greet(phrase: string) {
-    if (this.name) {
-      console.log(phrase + ' ' + this.name);
-    } else {
-      console.log('Hi');
-    }
+  loadCargo(amount: number) {
+    console.log('Loading cargo ... ' + amount);
   }
 }
 
-const person1: Greetable = new Person();
-person1.greet("Hi there, I'm");
-console.log(person1);
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // if ('loadCargo' in vehicle) {
+  //   vehicle.loadCargo(100);
+  // }
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(100);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+// DISCRAMINATED UNIONS
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+  }
+
+  console.log('Moving at speed: ' + speed);
+}
+
+moveAnimal({ type: 'bird', flyingSpeed: 100 });
+
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
+// const userInputElement = document.getElementById(
+//   'user-input'
+// )! as HTMLInputElement;
+const userInputElement = document.getElementById('user-input');
+
+// userInputElement.value = 'Hi There';
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = 'Hi There';
+}
+
+// INDEX PROPERTIES
+interface ErrorContainer {
+  // { email: 'Not a vaild email', username: 'Must start with a character' }
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email',
+  username: 'Must start with a character',
+};
+
+// OPTIONNAL CHAINING
+const fetchedUserData = {
+  id: 'u1',
+  name: 'Math',
+  job: { title: 'CEO', description: 'My own company' },
+};
+
+console.log(fetchedUserData?.job?.title);
+
+// NULLISH COALESCING
+// const userInput = null;
+const userInput = '';
+
+// ?? if this is null or undifined
+const storedData = userInput ?? 'DEFAULT';
+console.log(storedData);
