a5f49178a068327c80a2034b01b2830913c380f6
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-07-04 16:41:50 +0200

    ts namespaces

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,13 @@
+namespace App {
+  export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const orignalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = orignalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return adjDescriptor;
+  }
+}
