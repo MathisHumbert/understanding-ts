35ef804fb711643924d79074699aba3862b721f6
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-07-05 09:59:18 +0200

    es modules in ts

--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,13 +1,11 @@
-namespace App {
-  export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
-    const orignalMethod = descriptor.value;
-    const adjDescriptor: PropertyDescriptor = {
-      configurable: true,
-      get() {
-        const boundFn = orignalMethod.bind(this);
-        return boundFn;
-      },
-    };
-    return adjDescriptor;
-  }
+export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const orignalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = orignalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
 }
